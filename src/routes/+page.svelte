1cb2c1506e07f703589c457dc17359343be6055e
Mohamad Adithya <mohamadadithya@gmail.com>
2023-11-30 15:37:57 +0700

    update video test source

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,20 +5,12 @@
 <div class="grid place-items-center min-h-screen">
 	<Player
 		--primaryColor="red"
-		src="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8"
+		src="https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8"
 		disablepictureinpicture={true}
 		crossorigin="anonymous"
 		class="w-full max-w-4xl mx-auto"
 		configuration={{
 			abr: { defaultBandwidthEstimate: 10000000 }
 		}}
-		subtitles={[
-			{
-				label: 'English',
-				src: 'https://dl.dropboxusercontent.com/scl/fi/2yso1urlol8b95pz8c8i3/TOS-en.vtt?rlkey=02haff5d7dl7bxq7tcgvd895i&dl=0',
-				srclang: 'en',
-				kind: 'subtitles'
-			}
-		]}
 	/>
 </div>
